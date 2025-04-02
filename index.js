2ab4ede02afb089339431d87945ac2a363af92f8
Grazy241 <160822006+Grazy241@users.noreply.github.com>
2025-04-03 00:14:50 +0200

    niks werkt + het is laat + ik begin morgen opnieuw

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
+const apiURL = 'https://opendata.brussels.be/api/records/1.0/search/?dataset=cultureel-aanbod&q=';
 
 
+const locatielijst = document.getElementById('locatielijst');
+const instructie = document.getElementById('instructie');
 const favLijst = document.getElementById('favLijst');
-const themaswitch = document.getElementById('themaswitch');
+const themaswitch = document.getElementsByClassName('themaswitch');
 const sortBtn = document.getElementById('sortBtn');
 const searchInput = document.getElementById('searchInput');
+const favBtn = document.getElementsByClassName('favBtn');
 
+let locaties = [];
 let favorieten = JSON.parse(localStorage.getItem('favorieten')) || [];
 let sortAscending = true; 
 
+// api intialisen etc. //
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(apiURL);
+    if(!res.ok) throw new Error(`HTTP ${res.status}`);
+    const wrapper = await res.json();
+    const data = JSON.parse(wrapper.contents);
+  } catch (error) {
+    
+  }
+  
+}
 // sorteren //
 
 Filtered.sort((a,b) => sortAscending ? a.title.localCompare(b.title) : b.title.localCompare(a.title)
@@ -19,6 +37,19 @@ if (filtered.length === 0) {
   return;
 }
 
+// sorteerknop A-Z //
+sortBtn.addEventListener('click', () => {
+  sortAscending = !sortAscending;
+  sortBtn.textContent = sortAscending ? 'Sorteer A-Z' : 'Sorteer Z-A';
+  renderList();
+});
+
+themaswitch.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+});
+
+searchInput.addEventListener('input', renderList);
+
 // ❤️ - knop events
 
 const addFavoriet = (title) => {
@@ -29,7 +60,7 @@ const addFavoriet = (title) => {
   }
 };
 
-const renderFavoriet = () => {
+const renderFavorieten = () => {
   favLijst.innerHTML = '';
   favorieten.forEach(title => {
     const li = document.createElement('li');
@@ -41,6 +72,20 @@ const renderFavoriet = () => {
     favLijst.appendChild(li);
   });
 }
+
+const removeFavoriet = (title) => {
+  favorieten = favorieten.filter(fav => fav !== title);
+  localStorage.setItem('favorieten', JSON.stringify(favorieten));
+  renderFavorieten();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetchData();
+  updateFavList();
+})
+
+
+
 
 
 
